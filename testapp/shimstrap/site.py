b55e2ba14d4c4d0e348598181f8b2c28e45a7450
Joshua Horacsek <joshua_horacsek@sfu.ca>
2015-04-10 23:09:57 -0700

    Adding loader file

--- testapp/shimstrap/site.py
+++ testapp/shimstrap/site.py
@@ -3,4 +3,7 @@ from . import views
 
 urls = [
     url(r'^login/$', views.serve_shim, name='login')
+    # provide a raw map object
+    # provide a jquery override shim
+    # provide a require.js shim
 ]
