49efaa528d5eaae229b360eb6affdbcd5272a47c
Joshua Horacsek <joshua_horacsek@sfu.ca>
2015-05-10 17:41:37 -0700

    Adding JS code to bootstrap requests

--- testapp/shimstrap/__init__.py
+++ testapp/shimstrap/__init__.py
@@ -1,12 +1,40 @@
-from shimstrap.urls import AjaxTracker
+from django.core.urlresolvers import reverse, resolve
+from django.test import RequestFactory
+import hashlib
 
 
-def cache_object(request, name, dictionary_or_response):
+def cache_object(context, name, user=None, method="GET", kwargs=None, post_data=None):
+    factory = RequestFactory()
+    request = None
 
-    # Check to see if name corresponds to
-    if not AjaxTracker.Instance().has_name(name):
-        return
+    # Find the view
+    url = reverse(name, kwargs=kwargs)
+    view, args, kargs = resolve(url)
 
-    # Create a cache on this response if none exists
-    if not hasattr(request, '_ss_cached_objects'):
-        request._ss_cached_objects = {}
+    # Create a dummy request
+    if method == "POST":
+        request = factory.post(url, data=post_data)
+    else:
+        request = factory.get(url)
+
+    # Pass in the user
+    if user is not None:
+        request.user = user
+
+    # Call the view
+    response = view(request, *args, **kwargs)
+
+    # Create the cache if it doesn't exist
+    if 'ss_cache___' not in context:
+        context['ss_cache___'] = []
+
+    context['ss_cache___'] += [{
+        'hash_id': hashlib.sha1(url).hexdigest(),
+        'url': url,
+        'method': method,
+        'cached_data': response.content,
+        'name': name,
+        'type': response['Content-Type'],
+    }]
+
+    return context
